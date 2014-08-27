f84c25054b3ab852cf9a1e68af37494c598533fd
Mark Arneman <mark@purzue.com>
2014-08-27 10:41:41 -0400

    JsonTools extension

---
+++ Extension.php
@@ -0,0 +1,67 @@
+<?php
+/*
+* @Author: mark
+* @Date:   2014-05-07 14:19:04
+* @Last Modified by:   mark
+* @Last Modified time: 2014-05-07 15:24:42
+*/
+
+namespace Bearlikelion\TwigJsonTools;
+
+class Extension extends \Twig_Extension
+{
+	/**
+	 * Define Twig filters
+	 * @example
+	 * {{ string|json_decode }}
+	 * {{ string|json_encode }}
+	 * @return array
+	 */
+	public function getFilters()
+	{
+		return array(
+			new \Twig_SimpleFilter('json_decode', array($this, 'jsonDecode'))
+		);
+	}
+
+	/**
+	 * Define Twig functions
+	 * @example
+	 * {{ json_decode(string) }}
+	 * {{ json_encode(string) }}
+	 * @return array
+	 */
+	public function getFunctions()
+	{
+		return array(
+			'json_decode'  => new \Twig_Function_Method($this, 'jsonDecode'),
+			'json_encode' => new \Twig_Function_Method($this, 'jsonEncode')
+		);
+	}
+
+	/**
+	 * Decode JSON string
+	 * @param  string $string
+	 * @return object
+	 */
+	public function jsonDecode($string)
+	{
+		return json_decode($string);
+	}
+
+	/**
+	 * Encode an object or array to JSON
+	 * @param  array $array
+	 * @return string
+	 */
+	public function jsonEncode($array)
+	{
+		return json_encode($array);
+	}
+
+	/** Extension name */
+	public function getName()
+	{
+		return 'json_extension';
+	}
+}
